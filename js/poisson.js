fba44e50b20455ca3bac34918f93e143402acd06
PS-Jonathan <130980920+PS-Jonathan@users.noreply.github.com>
2024-04-09 14:40:50 +0200

    Update poisson.js
    Correction if(canvas) pour les pages RSE

--- js/poisson.js
+++ js/poisson.js
@@ -79,32 +79,33 @@ $(function() {
 
     var canvas = document.getElementById("canvas");
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    window.addEventListener('resize', function() {
+    if(canvas) {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-    });
-
-    var fishFlocking = new FishFlocking();
-    fishFlocking.setup(
-        canvas,
-        document.getElementById("poisson"),
-        document.getElementById("poisson_super")
-    );
-    fishFlocking.go();
-
-    HTMLCollection.prototype.forEach = Array.prototype.forEach;
-    document.getElementsByClassName("fish-option").forEach(function(element) {
-        element.oninput = function() {
-            fishFlocking.eval(element.id + " = " + this.value);
-            if (element.id === 'POPULATION') {
-                fishFlocking.eval('setupPopulation()');
-            }
-        };
-    });
-
+    
+        window.addEventListener('resize', function() {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+        });
+    
+        var fishFlocking = new FishFlocking();
+        fishFlocking.setup(
+            canvas,
+            document.getElementById("poisson"),
+            document.getElementById("poisson_super")
+        );
+        fishFlocking.go();
+    
+        HTMLCollection.prototype.forEach = Array.prototype.forEach;
+        document.getElementsByClassName("fish-option").forEach(function(element) {
+            element.oninput = function() {
+                fishFlocking.eval(element.id + " = " + this.value);
+                if (element.id === 'POPULATION') {
+                    fishFlocking.eval('setupPopulation()');
+                }
+            };
+        });
+    }
 });
 
 // ANIMATIONS ELEMENTS
